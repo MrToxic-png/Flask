c7a6e703ca794c41afd7ef0055a8ff2970dfab3d
ME <alexo.oozdvg@gmail.com>
2025-03-18 22:44:45 +0300

    mars4

--- main.py
+++ main.py
@@ -39,5 +39,33 @@ def image():
                 </html>'''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!DOCTYPE HTML>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+    </head>
+    <body>
+    <h1>
+    Жди нас, Марс!
+    </h1>
+    <img src="{url_for('static', filename='img/mars.png')}" 
+              alt="здесь должна была быть картинка, но не нашлась">
+    <div class="alert alert-primary" role="alert">
+    Человечество вырастает из детства
+    </div>
+    <div class="alert alert-secondary" role="alert">
+    Человечеству мала одна планета
+    </div>
+    <div class="alert alert-thirdly" role="alert">
+    Мы сделаем обитаемыми безжизненные пока планеты.
+    </div>
+    </body>'''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
