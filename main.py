933df369f7696f766a7dfcbbb8373f6d76a1b785
ME <alexo.oozdvg@gmail.com>
2025-03-20 00:31:32 +0300

    mars9

--- main.py
+++ main.py
@@ -321,5 +321,42 @@ def load_photo():
                 </html>'''
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''<!DOCTYPE HTML>
+<head>
+<meta charset="utf-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+</head>
+<body>
+<h1>Пейзажи марса</h1>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{url_for('static', filename='img/mars.png')}" height="450" class="d-block w-100" alt="хз">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='img/first.png')}" height="450" class="d-block w-100" alt="хз">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='img/second.png')}" height="450" class="d-block w-100" alt="хз">
+    </div>
+  </div>
+   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Предыдущий</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Следующий</span>
+    </button>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
