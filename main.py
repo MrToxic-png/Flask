929229856c58c26476800556e58df8f982f4643c
ME <alexo.oozdvg@gmail.com>
2025-03-20 23:04:16 +0300

    wtf4

--- main.py
+++ main.py
@@ -9,13 +9,31 @@ app.config['SECRET_KEY'] = 'my_wtf_key'
 def index(title):
     return render_template('base.html', title=title)
 
+
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
 
+
 @app.route('/list_prof/<lst>')
 def list_prof(lst):
     return render_template('professions.html', lst=lst)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def anwser():
+    slowar = {
+        'title': 'Анкета',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'выше среднего',
+        'profession': 'штурман',
+        'sex': 'male',
+        'motivation': 'застрять на марсе',
+        'ready': True
+    }
+    return render_template('auto_answer.html', **slowar)
 
 
 if __name__ == '__main__':
