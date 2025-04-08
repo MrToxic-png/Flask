21e2976526600ffd0eb61047d772ad936211539f
ME <alexo.oozdvg@gmail.com>
2025-04-08 15:24:00 +0300

    rest3

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, abort
 
 from . import db_session
 from .jobs import Jobs
@@ -12,6 +12,16 @@ blueprint = Blueprint(
     __name__,
     template_folder='templates'
 )
+
+
+@blueprint.errorhandler(404)
+def not_found(_):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@blueprint.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 @blueprint.route('/api/jobs')
@@ -30,14 +40,13 @@ def get_jobs():
 @blueprint.route('/api/jobs/<int:job_id>')
 def get_particular_jobs(job_id):
     if type(job_id) is not int:
-        return jsonify({'error': 'Invalid job id'})
+        abort(400)
     job = session.query(Jobs).filter_by(id=job_id).first()
-    if job:
-        dict_of_jobs = {'id': job.id, 'team_leader': job.team_leader, 'job': job.job, 'category': job.category,
-                        'is_finished': job.is_finished, 'work_size': job.work_size, 'collaborators': job.collaborators,
-                        'start_date': job.start_date, 'end_date': job.end_date}
+    if not job:
+        abort(404)
 
+    dict_of_jobs = {'id': job.id, 'team_leader': job.team_leader, 'job': job.job, 'category': job.category,
+                    'is_finished': job.is_finished, 'work_size': job.work_size, 'collaborators': job.collaborators,
+                    'start_date': job.start_date, 'end_date': job.end_date}
 
-        return jsonify([dict_of_jobs])
-    else:
-        return jsonify({'message': 'Job not found'})
+    return jsonify(dict_of_jobs)
---
+++ tests/test.py
@@ -0,0 +1,25 @@
+import requests
+
+def test_all_jobs():
+    response = requests.get('http://127.0.0.1:8080/api/jobs')
+    jsoned_jobs = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(jsoned_jobs, list)
+
+def test_particular_job():
+    response = requests.get('http://127.0.0.1:8080/api/jobs/1')
+    jsoned_job = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(jsoned_job, dict)
+
+def test_particular_job_id():
+    response = requests.get('http://127.0.0.1:8080/api/jobs/198267451098634590736587146395837265892734659874326590813')
+
+    assert response.status_code == 404
+
+def test_particular_job_input():
+    response = requests.get('http://127.0.0.1:8080/api/jobs/KiReal')
+
+    assert response.status_code == 400
